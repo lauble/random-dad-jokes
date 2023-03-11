19bb39c6450fcd06cfbde5f20305434178f2a2b0
Lauren Noble <lauren.noble3@gmail.com>
2023-03-11 13:41:54 -0500

    added while loop

--- dad-jokes.py
+++ dad-jokes.py
@@ -23,36 +23,45 @@ colors = (
 rand_color1 = choice(colors)
 rand_color2 = choice(colors)
 
+playing = True
+
 header = colored(figlet_format("dad jokes"), color=rand_color1)
 print(header)
 
-term = input("What would you like to search for? ")
-if not term:
-	term = input("Please try again. What would you like to search for? ")
-url = "https://icanhazdadjoke.com/search"
+while playing == True:
 
-res = requests.get(
-	url,
-	headers={"Accept": "application/json"},
-	params={"term": term}
-).json()
+	term = input("What would you like to search for? (press 'q' to quit) ")
 
-num_of_jokes = res["total_jokes"]
-results = res["results"]
+	if term == "q":
+		print("Thanks for playing!")
+		playing = False
 
-if num_of_jokes > 1:
-	print(f"There are {num_of_jokes} jokes about '{term}'. Here's one:")
-	joke = choice(results)["joke"]
-	print(colored(joke, color=rand_color2))
-elif num_of_jokes == 1:
-	print(f"I found one joke about '{term}':")
-	joke = results[0]['joke']
-	print(colored(joke, color=rand_color2))
-else:
-	msg = f"Sorry, there are no jokes about '{term}'."
-	print(colored(msg, color=rand_color2))
+	else:
 
+		if not term:
+			term = input("Please try again. What would you like to search for? ")
+		url = "https://icanhazdadjoke.com/search"
 
+		res = requests.get(
+			url,
+			headers={"Accept": "application/json"},
+			params={"term": term}
+		).json()
+
+		num_of_jokes = res["total_jokes"]
+		results = res["results"]
+
+		if num_of_jokes > 1:
+			print(f"There are {num_of_jokes} jokes about '{term}'. Here's one:")
+			joke = choice(results)["joke"]
+			print(colored(joke, color=rand_color2))
+		elif num_of_jokes == 1:
+			print(f"I found one joke about '{term}':")
+			joke = results[0]['joke']
+			print(colored(joke, color=rand_color2))
+		else:
+			msg = f"Sorry, there are no jokes about '{term}'."
+			print(colored(msg, color=rand_color2))
 
 
 
